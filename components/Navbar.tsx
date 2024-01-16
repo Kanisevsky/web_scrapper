d2ec46f6b50e3d022ad18245074e7799d17ac50d
Kanisevsky <meltsmokestudio@gmail.com>
2024-01-16 17:14:26 +0000

    navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,10 +1,27 @@
-import React from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
+
+const navIcons = [
+  {src: "/assets/icons/search.svg", alt:"search"},
+  {src: "/assets/icons/black-heart.svg", alt: "heart"},
+  {src: "/assets/icons/user.svg", alt: "user"}
+]
 
 function Navbar() {
   return (
-    <div>
-      Navbar
-    </div>
+    <header className='w-full'>
+      <nav className='nav'>
+        <Link href="/" className='flex item-center gap-1'>
+          <Image src="/assets/icons/logo.svg" alt="logo" width={27} height={27}/>
+          <p className='nav-logo'>
+            Price<span className='text-primary'>Wise</span>
+          </p>
+        </Link>
+        <div className="flex items-center gap-5">
+          {}
+        </div>
+      </nav>
+    </header>
   )
 }
 
