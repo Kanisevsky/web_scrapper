aa1dab552d7e9d375c956d1918d6e805fe921d3c
Kanisevsky <meltsmokestudio@gmail.com>
2024-01-17 17:37:50 +0000

    Home

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,21 @@
+import Image from "next/image";
 
  const Home = () => {
   return (
-   <div>
-    HomePage
-   </div>
+   <>
+    <section className="px-6 md:px-20 py-20 border-2 border-red-500">
+       <div className="flex max-xl:flex-col gap-16">
+        <div className="flex flex-col justify-center">
+          <p className="small-text">Smart Shopping Starts Here
+            <Image src="/assets/icons/arrow-right.svg" alt="arrow-right" width={16} height={16}/>
+          </p>
+            <h1 className="head-text">Unleash the Power of  
+              <span className="text-primary"> PriceWise</span>
+             </h1>
+        </div>
+       </div>
+    </section>
+   </>
   )
 }
 
