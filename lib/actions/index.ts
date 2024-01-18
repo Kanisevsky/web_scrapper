fd9c143946b7bb983bb33c25f033b7ef0c95b47d
Kanisevsky <meltsmokestudio@gmail.com>
2024-01-18 17:57:56 +0000

    server side scraper code

---
+++ lib/actions/index.ts
@@ -0,0 +1,11 @@
+"use server";
+import { scrapedAmazoneProduct } from "../scraper";
+export async function scrapeAndStoreProduct(productUrl: string) {
+  if (!productUrl) return;
+
+  try {
+    const scrapedProduct = await scrapedAmazoneProduct(productUrl);
+  } catch (error: any) {
+    throw new Error(`Failed to create/update product: ${error.message}`);
+  }
+}
---
+++ lib/scraper/index.ts
@@ -0,0 +1,3 @@
+export async function scrapedAmazoneProduct(url: string) {
+  if (!url) return;
+}
