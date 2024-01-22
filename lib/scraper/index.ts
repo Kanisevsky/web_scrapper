840f74b48f832dc8f51b083967455ee0c54c3cb4
Kanisevsky <meltsmokestudio@gmail.com>
2024-01-22 20:44:53 +0000

    scraping amazone product

--- lib/scraper/index.ts
+++ lib/scraper/index.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import * as cheerio from 'cheerio';
-import { extractPrice } from '../utils';
+import { extractPrice, extractCurrency } from '../utils';
 export async function scrapedAmazoneProduct(url: string) {
   if (!url) return;
 
@@ -36,10 +36,23 @@ export async function scrapedAmazoneProduct(url: string) {
     const outOfStock =
       $('#availability span').text().trim().toLowerCase() ===
       'currently unavailable';
-    const image =
+    const images =
       $('#imgBlkFront').attr('data-a-dynamic-image') ||
-      $('#landingImage').attr('data-a-dynamic-image');
-    console.log({ title, currentPrice, originalPrice, outOfStock, image });
+      $('#landingImage').attr('data-a-dynamic-image') ||
+      '{}';
+    const imageUrls = Object.keys(JSON.parse(images));
+    const currency = extractCurrency($('.a-price-symbol'));
+    const discountRate = $('.savingsPercentage').text().replace(/[-%]/g, '');
+
+    console.log({
+      title,
+      currentPrice,
+      originalPrice,
+      outOfStock,
+      imageUrls,
+      currency,
+      discountRate,
+    });
   } catch (error: any) {
     throw new Error(`Failed to scrape product: ${error.message}`);
   }
