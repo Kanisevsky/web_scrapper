68285ee12f21b34bddd3fbc0e50a5060f96883b5
Kanisevsky <meltsmokestudio@gmail.com>
2024-01-24 18:55:37 +0000

    Database setup

--- lib/actions/index.ts
+++ lib/actions/index.ts
@@ -8,6 +8,9 @@ export async function scrapeAndStoreProduct(productUrl: string) {
     connectToDB();
     const scrapedProduct = await scrapedAmazoneProduct(productUrl);
     if (!scrapedProduct) return;
+
+    let product = scrapedProduct;
+    // const existingProduct = await Product
   } catch (error: any) {
     throw new Error(`Failed to create/update product: ${error.message}`);
   }
