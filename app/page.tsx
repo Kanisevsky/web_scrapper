9d27874ed0d4e6189b39e891b582afa2b69567fd
Kanisevsky <meltsmokestudio@gmail.com>
2024-01-25 19:39:26 +0000

    Product detail page

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
 import HeroCarousel from "@/components/HeroCarousel";
 import SearchBar from "@/components/SearchBar";
 import Image from "next/image";
+import { getAllProducts } from "@/lib/actions";
 
- const Home = () => {
+ const Home = async () => {
+  const allProducts = await getAllProducts()
   return (
    <>
     <section className="px-6 md:px-20 py-24">
@@ -26,7 +28,7 @@ import Image from "next/image";
        <section className="trending-section">
           <h2 className="section-text">Trending</h2>
           <div className="flex flex-wrap gap-x-8 gap-y-16">
-            {["Apple Ipphone", "Book", "Sneakers"].map((product) => (
+            {allProducts?.map((product) => (
               <div key={product}>{product}</div>
             ))}
           </div>
