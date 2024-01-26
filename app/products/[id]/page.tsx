d711958d92aeba109fe26f11f180d8d8e72f2926
Kanisevsky <meltsmokestudio@gmail.com>
2024-01-26 22:42:40 +0000

    Product detail page

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -65,7 +65,35 @@ const ProductDetails = async ({params: {id}} : Props) => {
               {product.currency} {formatNumber(product.originalPrice)}
             </p>
           </div>
-          <p></p>
+          <div className="flex flex-col gap-4">
+            <div className="flex gap-3">
+              <div className="product-stars">
+                <Image
+                src="/assets/icons/star.svg"
+                alt="star"
+                width={16}
+                height={16}
+              />
+              <p className="text-sm text-primary-orange font-semibold">
+                {product.stars || "25"}
+              </p>
+              </div>
+              <div className="product-reviews">
+                <Image
+                src="/assets/icons/comment.svg"
+                alt="comment"
+                width={16}
+                height={16}
+              />
+              <p className="text-sm text-secondary font-semibold">
+                {product.reviewsCount} Reviews
+              </p>
+              </div>
+            </div>
+            <p className="text-sm text-black opacity-50">
+              <span className="text-primary-green font-semibold">93%</span> of buyers have recommended this
+            </p>
+          </div>
         </div>
       </div>
     </div>
