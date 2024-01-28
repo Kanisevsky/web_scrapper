00b5890a372c244c65fdb7befa18f324f4b55da2
Kanisevsky <meltsmokestudio@gmail.com>
2024-01-28 17:38:52 +0000

    Modal

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -28,6 +28,7 @@ function Modal() {
             >
               <Dialog.Overlay className="fixed inset-0"/>
             </Transition.Child>
+            <span className='inline-block h-screen align-middle' aria-hidden="true"/>
             <Transition.Child
             as={Fragment}
             enter='ease-out duration-300'
@@ -42,12 +43,49 @@ function Modal() {
                   <div className="flex justify-between">
                     <div className="p-3 border border-gray-200 rounded-10">
                       <Image
-                        src=""
-                        alt=""
+                        src="/assets/icons/logo.svg"
+                        alt="logo"
+                        width={28}
+                        height={28}
                       />
                     </div>
+                    <Image
+                        src="/assets/icons/x-close.svg"
+                        alt="close"
+                        width={24}
+                        height={24}
+                        className='cursor-pointer '
+                        onClick={closeModal}
+                      />
                   </div>
+                  <h4>
+                    Stay updated with product pricing alerts right in your inbox!
+                  </h4>
+                  <p>Never miss a bargain again with our timely alerts!</p>
                 </div>
+                <form className='flex flex-col mt-5 '>
+                  <label
+                    htmlFor='email'
+                    className='text-sm font-medium text-gray-700'
+                  >
+                    Email Address
+                  </label>
+                  <div className="dialog-input_container">
+                    <Image
+                      src="/assets/icons/mail.svg"
+                      alt="mail"
+                      width={18}
+                      height={18}
+                    />
+                    <input
+                      required
+                      type='email'
+                      id="email"
+                      placeholder='Enter Your Email Address'
+                      className='dialog-input'
+                    />
+                  </div>
+                </form>
               </div>
             </Transition.Child>
           </div>
