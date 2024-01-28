cb05889abb140ff87c8fc21476d3f43bd4a51ea2
Kanisevsky <meltsmokestudio@gmail.com>
2024-01-28 17:03:26 +0000

    Modal

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -1,10 +1,27 @@
-import React from 'react'
+"use client"
+import { useState, Fragment } from 'react'
+import { Dialog, Transition } from '@headlessui/react'
 
 function Modal() {
+  let [isOpen, setIsOpen] = useState(true)
+  const openModal = () => setIsOpen(true);
+  const closeModal = () => setIsOpen(false);
   return (
-    <div>
-      Modal
-    </div>
+    <>
+      <button
+        type='button'
+        className='btn'
+        onClick={openModal}
+      >Track
+      </button>
+      <Transition appear show={isOpen} as={Fragment}>
+        <Dialog as="div" onClose={closeModal} className="dialog-container">
+          
+        </Dialog>
+    </Transition>
+     
+    </>
+    
   )
 }
 
